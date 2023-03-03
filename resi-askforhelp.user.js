92a09d92e90dc78cf8ddf625c4247ec0f39b7ada
QuCla <122170839+QuCla@users.noreply.github.com>
2023-03-03 21:19:42 +0100

    edited name

--- resi-askforhelp.user.js
+++ resi-askforhelp.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Resi- Hilfeanfrage Chat
+// @name         ReSi- Hilfeanfrage Chat
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  shows current rank for rettungssimulator.online
